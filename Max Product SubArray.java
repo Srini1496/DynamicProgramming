8f3ecd7c7601b70191a8916dcd548ded4d56b393
Srini1496 <96612140+Srini1496@users.noreply.github.com>
2022-01-05 17:56:53 +0530

    Create Max Product SubArray.java

---
+++ Max Product SubArray.java
@@ -0,0 +1,55 @@
+/*
+Given an integer array nums, find a contiguous non-empty subarray within the array that has the largest product, and return the product.
+
+The test cases are generated so that the answer will fit in a 32-bit integer.
+
+A subarray is a contiguous subsequence of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+Example 2:
+
+Input: nums = [-2,0,-1]
+Output: 0
+Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+ 
+
+Constraints:
+
+1 <= nums.length <= 2 * 104
+-10 <= nums[i] <= 10
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+*/
+class Solution {
+    public int maxProduct(int[] nums) {
+       
+        int maxTemp=nums[0];
+        int minTemp=nums[0];
+        int max=nums[0];
+       
+        for(int i=1;i<nums.length;i++){
+             
+            if(nums[i]<0){
+              int temp=maxTemp;
+                maxTemp=minTemp;
+                minTemp=temp;
+            
+            }
+            
+            maxTemp=Math.max(maxTemp*nums[i],nums[i]);
+            
+            
+            minTemp=Math.min(minTemp*nums[i],nums[i]);
+            
+            
+            max=Math.max(max,maxTemp);
+        }
+        
+        return max;
+    }
+}
